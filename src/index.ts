37f17936fbedec7ba93507a284bcb19894ee3f3f
Guillem Blasco <gowet92@gmail.com>
2024-06-25 11:57:08 +0200

    Add main Typscript file

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+type Person = [name: string, age: number, isAdult: boolean, hairColor: string];
+type Pet = [
+  animalRace: string,
+  name: string,
+  age: number,
+  isPuppy: boolean,
+  hairColor: string
+];
